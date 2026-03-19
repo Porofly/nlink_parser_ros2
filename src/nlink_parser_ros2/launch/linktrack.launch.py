56ada6390f3e7d97aa0af79597ff586227646b59
fleshycat <crummycat0217@gmail.com>
2026-03-19 18:09:43 +0900

    feat: add support for multiple linktrack nodes with separate parameter files

--- src/nlink_parser_ros2/launch/linktrack.launch.py
+++ src/nlink_parser_ros2/launch/linktrack.launch.py
@@ -15,19 +15,34 @@ def generate_launch_description():
         ),
     ])
 
-    linktrack_param_file = os.path.join(
+    linktrack_tag0_param_file = os.path.join(
         get_package_share_directory('nlink_parser_ros2'),
         'params',
-        'linktrack_init_params.yaml'
+        'linktrack_init_tag0_params.yaml'
     )
 
-    linktrack_node = Node(
+    linktrack_tag1_param_file = os.path.join(
+        get_package_share_directory('nlink_parser_ros2'),
+        'params',
+        'linktrack_init_tag1_params.yaml'
+    )
+
+    linktrack_tag0_node = Node(
         package="nlink_parser_ros2",
         executable="linktrack",
         output="screen",
-        arguments=[linktrack_param_file],
-        parameters=[{ 'system_id': LaunchConfiguration('system_id') }]
+        arguments=[linktrack_tag0_param_file],
+        parameters=[{ 'system_id': LaunchConfiguration('system_id'), 'tag_id': 0 }]
     )
     
-    ld.add_action(linktrack_node)
+    linktrack_tag1_node = Node(
+        package="nlink_parser_ros2",
+        executable="linktrack",
+        output="screen",
+        arguments=[linktrack_tag1_param_file],
+        parameters=[{ 'system_id': LaunchConfiguration('system_id'), 'tag_id': 1 }]
+    )
+    
+    ld.add_action(linktrack_tag0_node)
+    ld.add_action(linktrack_tag1_node)
     return ld
